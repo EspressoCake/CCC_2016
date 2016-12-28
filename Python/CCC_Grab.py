93385ca90e0074d65befeaa48950bdd6b5722e67
JuJu <lucas.justin.k@gmail.com>
2016-12-28 15:19:05 -0500

    Regex Fix

--- Python/CCC_Grab.py
+++ Python/CCC_Grab.py
@@ -2,6 +2,8 @@ import requests
 from BeautifulSoup import BeautifulSoup
 import subprocess
 import os
+import re
+import sys
 
 def main():
     initial_URL = "http://mirror.netcologne.de/CCC/congress/2016/h264-hd/"
@@ -9,10 +11,12 @@ def main():
     soup = BeautifulSoup(request.text)
     urls = [a['href'] for a in soup.findAll('a', href=True) if "-eng-" in a['href'] and "-deu-" not in a['href']]
     for item in urls:
-        if os.path.isfile(item):
+        regex = re.compile("^33c3.\d+.eng.")
+        if os.path.isfile(re.sub(regex, "33C3_", item)):
             continue
         else:
+            print "Getting Item: %s" % (item)
             wget = initial_URL + item
-            os.system('aria2c -x16 %s' % (wget))
-            print item
+            os.system('aria2c -x16 %s -o %s >/dev/null 2>&1' % (wget, re.sub(regex, "33C3_", item)))
+
 main()
